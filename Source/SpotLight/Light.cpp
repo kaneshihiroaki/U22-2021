c2899150771eda58601b496f93d452dbef092d88
takayasu-masashi <65996541+takayasu-masashi@users.noreply.github.com>
2021-08-04 20:23:27 +0900

    ライトの移動追加

--- Source/SpotLight/Light.cpp
+++ Source/SpotLight/Light.cpp
@@ -7,10 +7,27 @@
 int PixelShaderHandle;
 int VertexShaderHandle;
 float LightRotateAngle;
+float LightRotateAngle2;
 int SpotLightHandle;
 int PointLightHandle;
+int DirLightHandle; 
 int i, j;
 float DrawX, DrawZ;
+
+int time = 600;
+int count;
+int cntFlg;
+float distance = 1200.0f;
+
+VECTOR dis;
+VECTOR dis1;
+VECTOR dis2;
+VECTOR dis3;
+VECTOR dis4;
+VECTOR dis5;
+VECTOR dis6;
+VECTOR dis7;
+VECTOR dis8;
 VECTOR LightPos;
 
 void Light_init() {
@@ -54,19 +71,146 @@ void Light_init() {
 	// ポイントライトのディフューズカラーを強い色にする
 	SetLightDifColorHandle(PointLightHandle, GetColorF(5.0f, 5.0f, 10.0f, 0.0f));
 
-	// グローバルアンビエントライト( 大域環境光 )を２０％の明るさにする
-	SetGlobalAmbientLight(GetColorF(0.2f, 0.2f, 0.2f, 0.0f));
-	
-	//ポイントライトの位置の更新
+	//ポイントライトの初期値を設定
 	SetLightPositionHandle(PointLightHandle, VGet(0.0f, 1000.0f, 0.0f));
+	
+	//// ディレクショナルライトを作成する
+	//DirLightHandle = CreateDirLightHandle(VGet(-1.0f, 0.0f, 0.0f));
+
+	//// ディレクショナルライトのアンビエントカラーを抑える
+	//SetLightAmbColorHandle(DirLightHandle, GetColorF(0.0f, 0.0f, 0.0f, 0.0f));
+
+	//// ディレクショナルライトのディフューズカラーを緑にする
+	//SetLightDifColorHandle(DirLightHandle, GetColorF(5.0f, 5.0f, 10.0f, 0.0f));
+	
+	// グローバルアンビエントライト( 大域環境光 )を２０％の明るさにする
+	SetGlobalAmbientLight(GetColorF(1.0f, 1.0f, 1.0f, 0.0f));
+
+	count = GetRand(8);
+	cntFlg = count;
+
+	dis = VGet(-distance, 0.0f, distance);
+	dis1 = VGet(0.0f, 0.0f, distance);
+	dis2 = VGet(distance, 0.0f, distance);
+	dis3 = VGet(-distance, 0.0f, 0.0f);
+	dis4 = VGet(0.0f, 0.0f, 0.0f);
+	dis5 = VGet(distance, 0.0f, 0.0f);
+	dis6 = VGet(-distance, 0.0f, -distance);
+	dis7 = VGet(0.0f, 0.0f, -distance);
+	dis8 = VGet(distance, 0.0f, -distance);
 }
-void Light() 
+void Light()
 {
+	//10秒経過したら方向転換
+	if (time < 600) {
+		time++;
+	}
+	else if (time >= 600) {
+		while (cntFlg == count) {
+				count = GetRand(8);
+			}
+		cntFlg = count;
+		time = 0;
+		//10秒間処理を止める
+		WaitTimer(10000);
+	}
+	if (count == 0) {
+		if (LightPos.x > dis.x) {
+			LightRotateAngle -= 0.01f;
+		}
+		if (LightPos.z > dis.z) {
+			LightRotateAngle2 += 0.01f;
+		}
+	}
+	else if (count == 1) {
+		if (LightPos.x < dis1.x) {
+			LightRotateAngle += 0.01f;
+		}
+		if (LightPos.x > dis1.x) {
+			LightRotateAngle -= 0.01f;
+		}
+		if (LightPos.z < dis1.z) {
+			LightRotateAngle2 += 0.01f;
+		}
+	}
+	else if (count == 2) {
+		if (LightPos.x < dis2.x) {
+			LightRotateAngle += 0.01f;
+		}
+		if (LightPos.z < dis2.z) {
+			LightRotateAngle2 += 0.01f;
+		}
+	}
+	else if (count == 3) {
+		if (LightPos.x > dis3.x) {
+			LightRotateAngle -= 0.01f;
+		}
+		if (LightPos.z > dis3.z) {
+			LightRotateAngle -= 0.01f;
+		}
+		if (LightPos.z < dis3.z) {
+			LightRotateAngle += 0.01f;
+		}
+	}
+	else if (count == 4) {
+		if (LightPos.x > dis4.x) {
+			LightRotateAngle -= 0.01f;
+		}
+		if (LightPos.x < dis4.x) {
+			LightRotateAngle += 0.01f;
+		}
+		if (LightPos.z > dis4.z) {
+			LightRotateAngle2 -= 0.01f;
+		}
+		if (LightPos.z < dis4.z) {
+			LightRotateAngle2 += 0.01f;
+		}
+	}
+	else if (count == 5) {
+		if (LightPos.x < dis5.x) {
+			LightRotateAngle += 0.01f;
+		}
+		if (LightPos.z < dis5.z) {
+			LightRotateAngle2 += 0.01f;
+		}
+		if (LightPos.z > dis5.z) {
+			LightRotateAngle2 -= 0.01f;
+		}
+	}
+	else if (count == 6) {
+		if (LightPos.x > dis6.x) {
+			LightRotateAngle -= 0.01f;
+		}
+		if (LightPos.z > dis6.z) {
+			LightRotateAngle2 -= 0.01f;
+		}
+	}
+	else if (count == 7) {
+		if (LightPos.x < dis7.x) {
+			LightRotateAngle += 0.01f;
+		}
+		if (LightPos.x > dis7.x) {
+			LightRotateAngle -= 0.01f;
+		}
+		if (LightPos.z > dis7.z) {
+			LightRotateAngle -= 0.01f;
+		}
+	}
+	else if (count == 8) {
+		if (LightPos.x < dis8.x) {
+			LightRotateAngle += 0.01f;
+		}
+		if (LightPos.z > dis8.z) {
+			LightRotateAngle2 -= 0.01f;
+		}
+	}
 	// ライトの位置の回転値を加算
-	LightRotateAngle += 0.01f;
+	LightPos = VGet(LightRotateAngle * 200.0f, 800.0f, LightRotateAngle2 * 200.0f);
 
-	LightPos = VGet(sin(-LightRotateAngle) * 200.0f, 800.0f, sin(-LightRotateAngle * 2) * 150.0f);
-
+	//10カウント表示
+	SetFontSize(100);
+	DrawFormatString(500, 10, 0x25525000, "%d秒",time/60);
+	
 	// スポットライトの位置の更新
 	SetLightPositionHandle(SpotLightHandle, LightPos);
 }
--- Source/SpotLight/Light.h
+++ Source/SpotLight/Light.h
@@ -1,14 +1,27 @@
 #pragma once
 
-extern int ModelHandle;
 extern int PixelShaderHandle;
 extern int VertexShaderHandle;
 extern float LightRotateAngle;
+extern float LightRotateAngle2;
 extern int SpotLightHandle;
 extern int PointLightHandle;
+extern int DirLightHandle;
 extern int i, j;
 extern float DrawX, DrawZ;
+extern int count;
+extern int cntFlg;
+
 extern VECTOR LightPos;
+extern VECTOR dis;
+extern VECTOR dis1;
+extern VECTOR dis2;
+extern VECTOR dis3;
+extern VECTOR dis4;
+extern VECTOR dis5;
+extern VECTOR dis6;
+extern VECTOR dis7;
+extern VECTOR dis8;
 
 void Light();
 void Light_init();
