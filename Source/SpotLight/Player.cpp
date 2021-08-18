a01fdc92314adb97110e4cf5d06cf21f9cbb8f2f
Yoshi696 <qiandaijicheng@gmail.com>
2021-08-18 18:22:31 +0900

    スタミナバーが０超えて減少するバグ直しました

--- Source/SpotLight/Player.cpp
+++ Source/SpotLight/Player.cpp
@@ -70,9 +70,9 @@ void PLAYER::Player_Controller() {
 	SetFontSize(18);
 	//DrawFormatString(10, 670, 0xFFFFFF, "スタミナ：%d / %d", c_StmCount, c_StmMax);
 	DrawFormatString(10, 650, 0xFFFFFF, "スタミナ");
-	DrawBox(10, 670, 220, 690, 0xFFFFFF,FALSE);
-	DrawBox(10, 670, 220 * c_StmCount / c_StmMax, 690, 0xFFFFFF,TRUE);
-
+	DrawBox(10, 670, 10+110, 690, 0xFFFFFF,FALSE);
+	DrawBox(10, 670, 10+110 * c_StmCount / c_StmMax, 690, 0xFFFFFF,TRUE);
+	printfDx("%d\n",c_StmCount);
 	//Player_Move(Sin, Cos);
 }
 
